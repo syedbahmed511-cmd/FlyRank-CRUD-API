aa3fbfa3ca0e99547a8f752a4efbff309d332267
Bilal <your-email@example.com>
2026-09-17 22:25:40 +0500

    Stage 04 Update & Delete Tasks

--- main.py
+++ main.py
@@ -27,20 +27,23 @@ class TaskCreate(BaseModel):
     title: str | None = None
 
 
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    done: bool | None = None
+
+
 @app.get("/")
 def home():
     return {
         "message": "Hello, FlyRank!",
         "project": "CRUD API",
-        "stage": 3
+        "stage": 4
     }
 
 
 @app.get("/health")
 def health_check():
-    return {
-        "status": "ok"
-    }
+    return {"status": "ok"}
 
 
 @app.get("/tasks")
@@ -54,19 +57,13 @@ def get_task(task_id: int):
         if task["id"] == task_id:
             return task
 
-    raise HTTPException(
-        status_code=404,
-        detail="Task not found"
-    )
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
 @app.post("/tasks", status_code=201)
 def create_task(task_data: TaskCreate):
     if task_data.title is None or not task_data.title.strip():
-        raise HTTPException(
-            status_code=400,
-            detail="Title is required"
-        )
+        raise HTTPException(status_code=400, detail="Title is required")
 
     new_task = {
         "id": max(task["id"] for task in tasks) + 1,
@@ -75,5 +72,36 @@ def create_task(task_data: TaskCreate):
     }
 
     tasks.append(new_task)
-
     return new_task
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task_data: TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+
+            if task_data.title is not None:
+                if not task_data.title.strip():
+                    raise HTTPException(
+                        status_code=400,
+                        detail="Title cannot be empty"
+                    )
+
+                task["title"] = task_data.title.strip()
+
+            if task_data.done is not None:
+                task["done"] = task_data.done
+
+            return task
+
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return
+
+    raise HTTPException(status_code=404, detail="Task not found")
