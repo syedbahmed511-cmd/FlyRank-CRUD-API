63784d761216633c4ca45aec5018827ab919b424
Bilal <your-email@example.com>
2026-09-17 21:44:48 +0500

    stage 02 Read Tasks

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Build CRUD API",
+        "done": False
+    },
+    {
+        "id": 3,
+        "title": "Upload project to GitHub",
+        "done": True
+    }
+]
 
 
 @app.get("/")
@@ -8,7 +27,7 @@ def home():
     return {
         "message": "Hello, FlyRank!",
         "project": "CRUD API",
-        "stage": 1
+        "stage": 2
     }
 
 
@@ -17,3 +36,20 @@ def health_check():
     return {
         "status": "ok"
     }
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
